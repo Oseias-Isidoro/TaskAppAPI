a40656f76ebfebf4127a2bbcd0156b72383fda02
Oseias Isidoro <oseiasisidoro@gmail.com>
2024-01-15 17:28:15 -0300

    update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static find($taskId)
+ * @method static create(array $taskDetails)
+ * @method static whereId($taskId)
+ * @method static whereUserId($userId)
+ */
 class Task extends Model
 {
     use HasFactory;
@@ -12,6 +18,7 @@ class Task extends Model
     protected $fillable = array(
         'description',
         'parent_task_id',
-        'status'
+        'status',
+        'user_id'
     );
 }
